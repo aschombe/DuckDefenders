b227c350ecba0e4d075dfa4bf74a6b5a351c0710
aschombe <aschombe@stevens.edu>
2022-12-07 14:55:56 -0500

    test

--- scripts/passcheck.js
+++ scripts/passcheck.js
@@ -9,22 +9,26 @@ let strengthBadge = document.getElementById('StrengthDisp')
 
 // The strong and weak password Regex pattern checker
 
-let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
-let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+// let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+// let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
 
-function StrengthChecker(PasswordParameter){
-    // We then change the badge's color and text based on the password strength
+// function StrengthChecker(PasswordParameter){
+//     // We then change the badge's color and text based on the password strength
 
-    if(strongPassword.test(PasswordParameter)) {
-        strengthBadge.style.backgroundColor = "green"
-        strengthBadge.textContent = 'Strong'
-    } else if(mediumPassword.test(PasswordParameter)){
-        strengthBadge.style.backgroundColor = 'blue'
-        strengthBadge.textContent = 'Medium'
-    } else{
-        strengthBadge.style.backgroundColor = 'red'
-        strengthBadge.textContent = 'Weak'
-    }
+//     if(strongPassword.test(PasswordParameter)) {
+//         strengthBadge.style.backgroundColor = "green"
+//         strengthBadge.textContent = 'Strong'
+//     } else if(mediumPassword.test(PasswordParameter)){
+//         strengthBadge.style.backgroundColor = 'blue'
+//         strengthBadge.textContent = 'Medium'
+//     } else{
+//         strengthBadge.style.backgroundColor = 'red'
+//         strengthBadge.textContent = 'Weak'
+//     }
+// }
+
+function StrengthChecker(password) {
+    
 }
 
 // Adding an input event listener when a user types to the  password input 
